c9a3168be951647d979f47c728386db03d297396
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2020-08-12 20:05:27 +0100

    Add a network controller and an RGB LED controller

---
+++ ESP32Toolset/Communication/NetworkController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using Windows.Devices.WiFi;
+
+namespace ESP32Toolset.Communication
+{
+    /// <summary>
+    /// Allows the simple control of a network connection
+    /// </summary>
+    public class NetworkController
+    {
+        /// <summary>
+        /// The automatically selected WiFi adapter used to connect to the network
+        /// </summary>
+        public readonly WiFiAdapter Adapter = WiFiAdapter.FindAllAdapters()[0];
+
+        /// <summary>
+        /// The network interface the controller is attached to
+        /// </summary>
+        public readonly NetworkInterface Interface;
+
+        /// <summary>
+        /// Instantiate a controller which automatically establishes a network connection
+        /// </summary>
+        /// <param name="networkSsid">The SSID to connect to</param>
+        /// <param name="networkPassword">The password for the network, if required</param>
+        public NetworkController(string networkSsid, string networkPassword = "")
+        {
+            Interface = NetworkInterface.GetAllNetworkInterfaces()[Adapter.NetworkInterface];
+
+            Adapter.ScanAsync();
+
+            WiFiAvailableNetwork requestedNetwork = null;
+
+            while (true)
+            {
+                while (Adapter.NetworkReport.AvailableNetworks.Length < 0)
+                {
+                    Thread.Sleep(100);
+                }
+
+                foreach (WiFiAvailableNetwork network in Adapter.NetworkReport.AvailableNetworks)
+                {
+                    if (network.Ssid.ToLower() == networkSsid.ToLower())
+                    {
+                        requestedNetwork = network;
+                    }
+                }
+
+                if (requestedNetwork != null)
+                {
+                    break;
+                }
+
+                Thread.Sleep(10000);
+
+                Adapter.ScanAsync();
+            }
+
+            WiFiConnectionResult connection = Adapter.Connect(requestedNetwork, WiFiReconnectionKind.Automatic, networkPassword);
+
+            if (connection.ConnectionStatus != WiFiConnectionStatus.Success)
+            {
+                throw new Exception($"Failed to connect to the requested network: {connection.ConnectionStatus.ToString()}");
+            }
+        }
+    }
+}
---
+++ ESP32Toolset/IO/RgbLed.cs
@@ -0,0 +1,49 @@
+﻿using Windows.Devices.Pwm;
+
+namespace ESP32Toolset.IO
+{
+    /// <summary>
+    /// Controls an RGB LED attached to 3 GPIO pins
+    /// </summary>
+    public class RgbLed
+    {
+        private readonly PwmPin _red;
+        private readonly PwmPin _green;
+        private readonly PwmPin _blue;
+
+        public RgbLed(PwmPin red, PwmPin green, PwmPin blue)
+        {
+            _red = red;
+            _green = green;
+            _blue = blue;
+
+            InitialisePins();
+        }
+
+        public RgbLed(int redPin, int greenPing, int bluePin)
+        {
+            _red = PwmController.GetDefault().OpenPin(redPin);
+            _green = PwmController.GetDefault().OpenPin(greenPing);
+            _blue = PwmController.GetDefault().OpenPin(bluePin);
+
+            InitialisePins();
+        }
+
+        private void InitialisePins()
+        {
+            _red.Stop();
+            _green.Stop();
+            _blue.Stop();
+
+            _red.SetActiveDutyCyclePercentage(0);
+            _green.SetActiveDutyCyclePercentage(0);
+            _blue.SetActiveDutyCyclePercentage(0);
+        }
+
+        public void SetRed(double brightness)
+        {
+            _red.Start();
+            _red.SetActiveDutyCyclePercentage(brightness);
+        }
+    }
+}
